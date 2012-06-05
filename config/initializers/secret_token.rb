7d1bd4cf93a2a706efb33cf522cc107eee216b5c
cdeliens <cdeliens@gmail.com>
2012-06-04 23:32:37 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VcesteSpine::Application.config.secret_token = 'ce390ef267ffe86974b76850af8b5cbe7692cd13e95d5f55da10d68c365e1015e5c1a56e3edcf55c80a1d579cd07a06b39fa32d99703b3d48172258c84d98454'
